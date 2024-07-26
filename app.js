e7f28de9eea34860327bd9cbe980110033881d32
Cristian Goyeneche <markaymarka.publicidad@hotmail.com>
2024-07-26 07:37:03 -0500

    barra de navegacion y enrutamiento

--- app.js
+++ app.js
@@ -9,18 +9,9 @@ app.set('views', __dirname + '/views');
 /* middleware para contenido estatico*/
 app.use(express.static(__dirname + '/public'));
 
+/* rutas web */
+app.use('/', require('./router/rutasweb'));
 
-/*enrutamiento*/
-app.get('/', (req, res) => {
-    // res.send('!!!!Hello World!')
-    res.render('index', {titulo: "Bienvenido a Node.js con Express y con EJS"});
-})
-
-/*ejemplo de enrutamiento */
-app.get("/servicios", (req, res) => {
-    //res.send("Bienvenido a la pagina de servicios");
-    res.render('index', {titulo: "Nuestros servicios Dinamicos"});
-});
 
 /* direccionar a vista 404 cuando se presente el error 404 */
 app.use((req, res, next)=>{
---
+++ router/rutasweb.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router();
+
+/*enrutamiento*/
+router.get('/', (req, res) => {
+    // res.send('!!!!Hello World!')
+    res.render('index', {titulo: "Bienvenido a Node.js con Express y con EJS"});
+})
+
+/*ejemplo de enrutamiento */
+router.get("/servicios", (req, res) => {
+    res.render('index', {titulo: "Nuestros servicios Dinamicos"});
+});
+
+modulo.exports = router;
