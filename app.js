f7c0d279fa20863c6a09d4f91618a2c5f5360fc5
Cristian Goyeneche <markaymarka.publicidad@hotmail.com>
2024-07-30 08:05:25 -0500

    trabajo dia masrtes

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+/* conexion a la base de datos */
+const mongoose = require('mongoose');
+const usuario = 'crisgstack6am';
+const password = '9DtoUyxtgl4idgc';
+const dbName = 'mascotas';
+
+const uri = `mongodb+srv://${usuario}:${password}@cluster0.ile9mkh.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`
+mongoose.connect(uri)
+  .then(()=> console.log('conectado a MongoDB')) 
+  .catch(e=> console.log('error de conexión a MongoDB', e))
+
 /* Establecemos el motor de plantillas */
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
