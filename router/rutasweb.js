4ef85997cdf5822a2a024274b5ab58de3d11617d
Cristian Goyeneche <markaymarka.publicidad@hotmail.com>
2024-07-29 21:22:49 -0500

    arreglo de daño

--- router/rutasweb.js
+++ router/rutasweb.js
@@ -3,13 +3,12 @@ const router = express.Router();
 
 /*enrutamiento*/
 router.get('/', (req, res) => {
-    // res.send('!!!!Hello World!')
     res.render('index', {titulo: "Bienvenido a Node.js con Express y con EJS"});
 })
 
 /*ejemplo de enrutamiento */
-router.get("/servicios", (req, res) => {
-    res.render('index', {titulo: "Nuestros servicios Dinamicos"});
+router.get("/servicios", (req, res)=>{
+    res.render('servicios', {titulo: "Nuestros servicios Dinamicos"});
 });
 
 module.exports = router;
