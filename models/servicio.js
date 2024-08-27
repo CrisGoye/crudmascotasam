27c96b3fd722bd876874aca2904057e1523ede3b
Cristian Goyeneche <markaymarka.publicidad@hotmail.com>
2024-08-27 07:03:39 -0500

    CRUD FINAL

--- models/servicio.js
+++ models/servicio.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const servicioSchema = new Schema({
-    nombre: String,
+    servicio: String,
     descripcion: String
 });
 
